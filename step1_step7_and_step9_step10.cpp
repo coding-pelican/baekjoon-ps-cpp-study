21a4beac5fe44a1dcd41f79c53fc1a2b6b86f40d
Gyeongtae Kim <codingpelican@gmail.com>
2022-05-01 02:32:11 +0900

    Update step1_step7_and_step9_step10.cpp

--- step1_step7_and_step9_step10.cpp
+++ step1_step7_and_step9_step10.cpp
@@ -1118,6 +1118,41 @@ int main() {
     return 0;
 }
 */
+// 재귀함수가 뭔가요? 17478
+/*
+#include <bits/stdc++.h>
+using namespace std;
+int N;
+void teachRecursion(int depth) {
+    for (int i = 0; i < depth; i++) cout << "____";
+    cout << "\"재귀함수가 뭔가요?\"\n";
+
+    if (depth == N) {
+        for (int i = 0; i < depth; i++) cout << "____";
+        cout << "\"재귀함수는 자기 자신을 호출하는 함수라네\"\n";
+        for (int i = 0; i < depth; i++) cout << "____";
+        cout << "라고 답변하였지.\n";
+        return;
+    }
+
+    for (int i = 0; i < depth; i++) cout << "____";
+    cout << "\"잘 들어보게. 옛날옛날 한 산 꼭대기에 이세상 모든 지식을 통달한 선인이 있었어.\n";
+    for (int i = 0; i < depth; i++) cout << "____";
+    cout << "마을 사람들은 모두 그 선인에게 수많은 질문을 했고, 모두 지혜롭게 대답해 주었지.\n";
+    for (int i = 0; i < depth; i++) cout << "____";
+    cout << "그의 답은 대부분 옳았다고 하네. 그런데 어느 날, 그 선인에게 한 선비가 찾아와서 물었어.\"\n";
+
+    teachRecursion(depth + 1);
+
+    for (int i = 0; i < depth; i++) cout << "____";
+    cout << "라고 답변하였지.\n";
+}
+int main(void) {
+    cin >> N;
+    puts("어느 한 컴퓨터공학과 학생이 유명한 교수님을 찾아가 물었다.");
+    teachRecursion(0);
+}
+*/
 // 별 찍기 - 10 2447
 /*
 #include <bits/stdc++.h>
@@ -1165,6 +1200,7 @@ int main(void) {
     hanoi(n, 1, 2, 3);
 }
 */
+// ----구현----
 //음계 2920
 /*
 #include <bits/stdc++.h>
@@ -1199,7 +1235,6 @@ int main(void) {
 // 블랙잭 2798
 /*
 #include <bits/stdc++.h>
-
 #include <algorithm>
 using namespace std;
 int card[100] = {0};
@@ -1218,6 +1253,31 @@ int main(void) {
     cout << result << '\n';
 }
 */
+// 분해합 2231
+/*
+#include <bits/stdc++.h>
+using namespace std;
+int main(void) {
+    int N, n_digit, sum = 0;
+    cin >> N;
+    for (int i = 1; i < N; ++i) {
+        sum = i;
+        n_digit = i;
+        while (n_digit) {
+            sum += n_digit % 10;
+            n_digit /= 10;
+        }
+        if (N == sum) {
+            cout << i << '\n';
+            return 0;
+        }
+    }
+    cout << '0' << '\n';
+}
+*/
+// 덩치 7568
+// 체스판 다시 칠하기 1018
+// 영화감독 숌 1436
 // -----정렬-----
 // 수 정렬하기 2750, 수 정렬하기 2 2751
 /*
